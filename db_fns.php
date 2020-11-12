3773641eaa7c257f3940a5415c8243509b218e30
Web-bisnes <74204522+Web-bisnes@users.noreply.github.com>
2020-11-12 15:00:58 +0200

    Update db_fns.php

--- db_fns.php
+++ db_fns.php
@@ -7,12 +7,17 @@ define('DB_USER',$user);
 define('DB_SERVER',$server);
 
 function db_connect() {
+static $result;
+if (!isset($result) or empty($result)){
    $result = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
    if (!$result) {
-     throw new Exception('Could not connect to database server');
-   } else {
-     return $result;
+      throw new Exception('Could not connect to database server');
+      } 
+   else {
+      return $result;
+      }
    }
+return $result;
 }
 
 ?>
